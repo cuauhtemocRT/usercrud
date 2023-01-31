bbf3df5dece61d6fe73dce8bca091eaad37659b4
Cuauhtemoc Rodriguez <1979cuau@gmail.com>
2023-01-30 22:10:30 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import FormUser from './components/FormUser'
+import UserCard from './components/UserCard'
+import useCrud from './hooks/useCrud'
+
+function App() {
+
+  const [closeForm, setCloseForm] = useState(true)
+
+  const {users, getAllUsers, createNewUser, deleteUserById, updateUserById} = useCrud()
+
+  const [updateInfo, setUpdateInfo] = useState()
+
+
+  useEffect(() => {
+    getAllUsers()
+  }, [])
+
+
+
+  return (
+    <div className="App">
+      <h1 className="App__title">Users</h1>
+      <button onClick={() => setCloseForm(false)} className="App__btn">+ New user</button>
+      <div className={`form-container ${closeForm && `close__form`}`}>
+      <FormUser 
+        createNewUser={createNewUser}
+        updateInfo={updateInfo}
+        updateUserById={updateUserById}
+        setUpdateInfo={setUpdateInfo}
+        setCloseForm={setCloseForm}
+      />
+      </div>
+      <div className='user-container'>
+      {
+        users?.map(user => (
+          <UserCard 
+            key={user.id}
+            user={user}
+            deleteUserById={deleteUserById}
+            setUpdateInfo={setUpdateInfo}
+            setCloseForm={setCloseForm}
+          />
+        ))
+      }
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/FormUser.jsx
@@ -0,0 +1,66 @@
+import React, {useEffect} from 'react'
+import { useForm } from 'react-hook-form'
+import './styles/formUser.css'
+
+const FormUser = ({createNewUser, updateInfo, updateUserById, setUpdateInfo, setCloseForm}) => {
+
+    const {register, reset, handleSubmit} = useForm()
+
+    console.log(updateInfo)
+
+    useEffect(() => {
+            reset(updateInfo)
+    }, [updateInfo])
+    
+
+    const submit = data => {
+
+        if (updateInfo) {
+            //update
+          updateUserById(updateInfo.id, data)
+          setUpdateInfo()
+        } else {
+            //Create
+          createNewUser(data)
+        }
+        
+        setCloseForm(true)
+        reset({
+            email: "",
+            first_name: "",
+            last_name: "",
+            password: "",
+            birthday: ""
+        })
+    }
+
+  return (
+    <form className="form" onSubmit={handleSubmit(submit)}>
+        <div onClick={() => setCloseForm(true)} className="form__x">x</div>
+        <h2 className="form__title">{updateInfo ? 'Update User' : 'Create User'}</h2>
+        <div className="form__div">
+          <label className="form__label" htmlFor="email">Email</label>
+          <input className="form__input" type="email" id="email" {...register("email")}/>
+        </div>
+        <div className="form__div">
+          <label className="form__label" htmlFor="password">Password</label>
+          <input className="form__input" type="password" id="password" {...register("password")}/>
+        </div>
+        <div className="form__div">
+          <label className="form__label" htmlFor="first_name">First_name</label>
+          <input className="form__input" type="text" id="first_name" {...register("first_name")}/>
+        </div>
+        <div className="form__div">
+          <label className="form__label" htmlFor="last_name">Last_name</label>
+          <input className="form__input" type="text" id="last_name" {...register("last_name")}/>
+        </div>
+        <div className="form__div">
+          <label className="form__label" htmlFor="birthday">Birthday</label>
+          <input className="form__input" type="date" id="birthday" {...register("birthday")}/>
+        </div>
+        <button className="form__btn">Submit</button>
+    </form>
+  )
+}
+
+export default FormUser
